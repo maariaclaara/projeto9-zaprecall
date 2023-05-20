8c4dbe6d2d76f5768136505054064a4be3f4eef3
Maria Clara <129561798+maariaclaara@users.noreply.github.com>
2023-05-20 01:38:14 -0300

    TOP OK / INICIO FLASH / INICIO BOTTOM

---
+++ Top.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
+
+export default function Top() {
+
+    return (
+        <ContainerTop>
+            <Image src={logo} alt={logo} />
+            <Title>ZapRecall</Title>
+        </ContainerTop>
+    );
+}
+
+const ContainerTop = styled.div`
+    height: 153px;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    bottom: auto;
+    z-index: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Image = styled.img`  
+    width: 52px;
+    height: 60px;
+    margin-right: 10px;
+`;
+
+const Title = styled.h1`
+    width: 203px;
+    height: 44px;
+    font-family: 'Righteous', cursive;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 45px;
+    letter-spacing: -0.012em;
+    color: #FFFFFF;
+    transform: rotate(0.58deg);
+    text-align: center;
+`;
