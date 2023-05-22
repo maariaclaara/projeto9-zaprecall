7e8c04ade5bdfc0063afe38e103f7ba01adeba40
Maria Clara <129561798+maariaclaara@users.noreply.github.com>
2023-05-22 11:59:14 -0300

    ITENS COMPLETOS / INICIO DE DATA-TESTE

--- Bottom.jsx
+++ Bottom.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
-/*import Response from "./Response";*/
+import styled from "styled-components";
 
-export default function Bottom() {
+
+export default function Bottom({ counter }) {
 
     return (
-    <>
-        <h1>BOTTOM</h1>
-    </>
+    <ContainerCount>
+        <Divcounter>
+            <Concluded data-test="footer"> {counter}/8 CONCLUÍDOS </Concluded>
+        </Divcounter>
+    </ContainerCount>
     );
 }
+
+const ContainerCount = styled.div `
+    width: 100%;
+    height: 70px;
+    background-color: #FFFFFF;
+    box-shadow: 0px -4px 6px rgba(0, 0, 0, 0.05);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    bottom: 0;
+    top: auto;
+    z-index: 2;
+`;
+
+const Divcounter = styled.div `
+    width: 200px;
+    height: 22px;
+    background-color: #FFFFFF;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Concluded = styled.h1 `
+    font-family: 'Recursive';
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
+    color: #333333; 
+`;
